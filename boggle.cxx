af038af638d9e67724f07db945930c73190dd8b7
Mintzmyer <samimintzmyer@gmail.com>
2017-07-13 11:50:25 -0700

    Outline of Cube and Cubie classes

--- boggle.cxx
+++ boggle.cxx
@@ -54,21 +54,172 @@ class Cubie
     int** nextTo;
     
     public:
-        Cubie(int, char);
+        setChar(char);
+        setNeighbors(int);
 };
 
-Cubie::Cubie(int inNeighbors, char inLetter)
+Cubie::setChar(char inLetter)
 {
-    neighbors = inNeighbors;
     letter = inLetter;
-    int* a[neighbors];
-    nextTo = a;
+}
+
+Cubie::setNeighbors(int inNeighbors)
+{ 
+    neighbors = inNeighbors;
+    int* array[neighbors];
+    nextTo = array;
 }
 
 class Cube
 {
+    int size;
+    int** Cubies;
 
+    public:
+        Cube(int);
+        printCube();
 };
+
+/*****************************************
+    Initialize Cube
+    4 Cubie 'types' to consider:
+    Central    -     26 Connections
+    Side       -     17 Connections
+    Edge       -     11 Connections
+    Corner     -     7 Connections
+*****************************************/
+Cube::Cube(int inSize)
+{
+    size = inSize;
+    int* array[size*size*size]
+    Cubies = array;
+
+    // Create sizeXsizeXsize cubies for the cube
+    for (int c = 0; c < size*size*size; c++)
+    {
+        Cubie cell;
+        Cubies[c] = cell;
+    }
+
+    //  Populate cubie nextTo array with pointer to all cubies neighboring
+    //  Iterate through the ith layer of the cube
+    for (int i = 0; i < size; i++)
+    {
+        //  Iterate through the jth cubie of the ith layer
+        for (int j = 0; j < size*size; j++)
+        {
+            //  If layer is 'front' or 'back' face of cube
+       	    if (i == 0 || i == size-1)
+            {
+                //  If cubie is on top row of front/back face
+                if (j < size)
+                {
+                    if (j % size == 0)
+                    {
+                    //  Top Left Corner Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Top Right Corner Found!
+                    }
+                    else
+                    {
+                    //  Top Edge Found!
+                    }
+                }
+                //  If cubie is on bottom row of front/back face
+                elif (j >= size*(size-1))
+                {
+                    if (j % size == 0)
+                    {
+                    //  Bottom Left Corner Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Bottom Right Corner Found!
+                    }
+                    else
+                    {
+                    //  Bottom Edge Found!
+                    }
+                }
+                else
+                {
+                //  If cubie is on middle side of front/back face
+                    if (j % size == 0)
+                    {
+                    //  Left Edge Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Right Edge Found!
+                    }
+                    else
+                    {
+                    //  Side Found!
+                    }
+                }
+            }
+            //  If layer is 'middle' faces of cube sandwich
+            else
+            {
+                //  If cubie is on top row of middle faces
+                if (j < size)
+                {
+                    if (j % size == 0)
+                    {
+                    //  Top Left Edge Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Top Right Edge Found!
+                    }
+                    else
+                    {
+                    //  Top Side Found!
+                    }
+                }
+                //  If cubie is on bottom row of middle faces
+                elif (j >= size*(size-1))
+                {
+                    if (j % size == 0)
+                    {
+                    //  Bottom Left Edge Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Bottom Right Edge Found!
+                    }
+                    else
+                    {
+                    //  Bottom Side Found!
+                    }
+                }
+                else
+                {
+                //  If cubie is on middle side of middle faces
+                    if (j % size == 0)
+                    {
+                    //  Left Side Found!
+                    }
+                    if (j+1 % size == 0)
+                    {
+                    //  Right Side Found!
+                    }
+                    else
+                    {
+                    //  Central Found!
+                    }
+                }
+            }
+        }
+    }
+}
+
+Cube::printCube()
+{
+
+}
 
 /*************************************
     Method for word search
