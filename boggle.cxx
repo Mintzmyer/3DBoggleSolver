7d12deebe71508d82fcd7ebd634699e4fb2d0fc9
Mintzmyer <samimintzmyer@gmail.com>
2017-07-13 09:22:41 -0700

    boggle solver outline

---
+++ boggle.cxx
@@ -0,0 +1,45 @@
+/***********************************************
+    3D Boggle Solver
+    for Connected Signals hiring process
+    
+    Samantha Mintzmyer
+***********************************************/
+
+/*************************************
+    Data Structure for Cubies and Cube
+    Class for Cubies and Cube
+*************************************/
+
+
+
+/*************************************
+    Method for dictionary
+    Uploads words to hashtable
+*************************************/
+
+
+
+/*************************************
+    Method for word search
+    Traverses cube comparing to hasht
+*************************************/
+
+
+
+/*************************************
+    Main: 
+    Calls dictUp, Calls travCube
+*************************************/
+
+//  Call method for dictionary
+
+//  Iterate over list of cubes
+
+    //  Upload cube
+
+    //  Traverse cube
+
+// Print # of cubes and time
+
+
+
